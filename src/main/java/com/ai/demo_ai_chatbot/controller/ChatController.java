ffe0956a34fa2f19db63d2909e829fc0aa6aa82b
saba akhtar <sabasaeedakhtar@gmail.com>
2025-07-23 14:06:05 +1000

    enabled crossorigin

--- src/main/java/com/ai/demo_ai_chatbot/controller/ChatController.java
+++ src/main/java/com/ai/demo_ai_chatbot/controller/ChatController.java
@@ -5,14 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Slf4j
+@CrossOrigin(origins="*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/bot")
@@ -25,6 +23,7 @@ public class ChatController {
         log.info("The request looks like {}", query);
         String request = query.get("text");
         String response = chatService.gettingAnswer(request);
+        log.info("The response is {}",response);
         return ResponseEntity.ok(response);
     }
 
